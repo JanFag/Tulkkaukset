3469d946f0164e7019e7f87a34f7c0bf07e4c08b
Janica <27jank@gmail.com>
2020-03-01 12:48:59 +0200

    create commit

---
+++ src/main/java/projekti/tulkkaukset/TulkkauksetApplication.java
@@ -0,0 +1,13 @@
+package projekti.tulkkaukset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulkkauksetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulkkauksetApplication.class, args);
+	}
+
+}
