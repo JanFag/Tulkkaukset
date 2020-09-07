b68e51f9a747f79dbffc59fcdaf58b040d7f8d7e
Janica <27jank@gmail.com>
2020-09-07 21:40:14 +0300

    domain update

--- src/main/java/projekti/tulkkaukset/domain/Tulkkaus.java
+++ src/main/java/projekti/tulkkaukset/domain/Tulkkaus.java
@@ -1,5 +1,11 @@
 package projekti.tulkkaukset.domain;
 
 public class Tulkkaus {
-
+	private long id;
+	private String tilaaja;
+	private String tulkkausPaikka;
+	private String aihe;
+	private String tulkkausTyyppi;
+	private String pvm;
+	
 }
