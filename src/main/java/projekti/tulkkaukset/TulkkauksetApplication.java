3919c92c4cfc9874226e2b0945bd6e9a7cd49098
Janica <27jank@gmail.com>
2020-10-11 19:32:27 +0300

    update

--- src/main/java/projekti/tulkkaukset/TulkkauksetApplication.java
+++ src/main/java/projekti/tulkkaukset/TulkkauksetApplication.java
@@ -3,10 +3,12 @@ package projekti.tulkkaukset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TulkkauksetApplication {
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class TulkkauksetApplication  {
+
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TulkkauksetApplication.class, args);
 	}
 
